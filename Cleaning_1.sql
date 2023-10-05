8c9c08917491a1a36bd6106e60a7d89d300a3569
JRLim0902 <83488769+JRLim0902@users.noreply.github.com>
2023-10-05 21:38:16 +0800

    Update Cleaning_1.sql

--- Cleaning_1.sql
+++ Cleaning_1.sql
@@ -1,88 +1,88 @@
 -- Data cleaning: separate amenities from Name 
---Current Name format: name · rating · amenities OR name · amenities
-select name from dbo.singapore_listings
+--Current Name format: name Â· rating Â· amenities OR name Â· amenities
+SELECT name FROM dbo.singapore_listings
 
 -- Separating name into name & features (aka rating & amenities)
 SELECT
 name,
-SUBSTRING(name, 1, CHARINDEX('·', name) -1 ) as NewName
-, SUBSTRING(name, CHARINDEX('·', name) + 1 , LEN(name)) as Features
-From dbo.singapore_listings
+SUBSTRING(name, 1, CHARINDEX('Â·', name) -1 ) as NewName
+, SUBSTRING(name, CHARINDEX('Â·', name) + 1 , LEN(name)) as Features
+FROM dbo.singapore_listings
 
 -- Create new Column Features
 ALTER TABLE singapore_listings
-Add Features Nvarchar(255);
+ADD Features Nvarchar(255);
 
-Update singapore_listings
-SET Features = SUBSTRING(name, CHARINDEX('·', name) + 1 , LEN(name))
+UPDATE singapore_listings
+SET Features = SUBSTRING(name, CHARINDEX('Â·', name) + 1 , LEN(name))
 
 -- separate ratings from features
-select
+SELECT
 CASE
-	WHEN Features LIKE '%!%' THEN SUBSTRING(Features, CHARINDEX('·', Features) + 1 , LEN(Features))
+	WHEN Features LIKE '%!%' THEN SUBSTRING(Features, CHARINDEX('Â·', Features) + 1 , LEN(Features))
 	ELSE Features
 END AS NewF
-From dbo.singapore_listings
+FROM dbo.singapore_listings
 
 -- Create column Amenities
 ALTER TABLE singapore_listings
-Add Amenities Nvarchar(255);
+ADD Amenities Nvarchar(255);
 
-Update singapore_listings
+UPDATE singapore_listings
 SET Amenities = 
 (CASE
-	WHEN Features LIKE '%!%' THEN SUBSTRING(Features, CHARINDEX('·', Features) + 1 , LEN(Features))
+	WHEN Features LIKE '%!%' THEN SUBSTRING(Features, CHARINDEX('Â·', Features) + 1 , LEN(Features))
 	ELSE Features
 END)
 
 --select name, amenities
 --from dbo.singapore_listings
 
-select REPLACE(amenities, '.', 'point')
-from dbo.singapore_listings
+SELECT REPLACE(amenities, '.', 'point')
+FROM dbo.singapore_listings
 
-Update singapore_listings
+UPDATE singapore_listings
 SET Amenities = REPLACE(amenities, '.', 'point')
 
 -- Separate amenities into diff columns
-Select
+SELECT
 amenities
-,PARSENAME(REPLACE(amenities, '·', '.') , 3) as bedroom
-,PARSENAME(REPLACE(amenities, '·', '.') , 2) as bed
-,PARSENAME(REPLACE(amenities, '·', '.') , 1) as bath
-From dbo.singapore_listings
+,PARSENAME(REPLACE(amenities, 'Â·', '.') , 3) AS bedroom
+,PARSENAME(REPLACE(amenities, 'Â·', '.') , 2) AS bed
+,PARSENAME(REPLACE(amenities, 'Â·', '.') , 1) AS bath
+FROM dbo.singapore_listings
 
-select distinct PARSENAME(REPLACE(amenities, '·', '.') , 3) as bedroom
-From dbo.singapore_listings
+SELECT DISTINCT PARSENAME(REPLACE(amenities, 'Â·', '.') , 3) AS bedroom
+FROM dbo.singapore_listings
 
-select distinct PARSENAME(REPLACE(amenities, '·', '.') , 2) as bed
-From dbo.singapore_listings
-order by bed
+SELECT DISTINCT PARSENAME(REPLACE(amenities, 'Â·', '.') , 2) AS bed
+FROM dbo.singapore_listings
+ORDER BY bed
 
-select distinct PARSENAME(REPLACE(amenities, '·', '.') , 1) as bath
-From dbo.singapore_listings
-
-ALTER TABLE singapore_listings
-Add Bedroom Nvarchar(255);
-
-Update singapore_listings
-SET bedroom = PARSENAME(REPLACE(amenities, '·', '.') , 3)
+SELECT DISTINCT PARSENAME(REPLACE(amenities, 'Â·', '.') , 1) AS bath
+FROM dbo.singapore_listings
 
 ALTER TABLE singapore_listings
-Add Bed Nvarchar(255);
+ADD Bedroom Nvarchar(255);
 
-Update singapore_listings
-SET bed = PARSENAME(REPLACE(amenities, '·', '.') , 2)
+UPDATE singapore_listings
+SET bedroom = PARSENAME(REPLACE(amenities, 'Â·', '.') , 3)
 
 ALTER TABLE singapore_listings
-Add Bath Nvarchar(255);
+ADD Bed Nvarchar(255);
 
-Update singapore_listings
-SET bath = PARSENAME(REPLACE(amenities, '·', '.') , 1)
+UPDATE singapore_listings
+SET bed = PARSENAME(REPLACE(amenities, 'Â·', '.') , 2)
 
-select name, bedroom, bed, bath
-from dbo.singapore_listings
+ALTER TABLE singapore_listings
+ADD Bath Nvarchar(255);
 
-select name, bedroom
-from dbo.singapore_listings
-where bedroom not in ('%bedroom%')
+UPDATE singapore_listings
+SET bath = PARSENAME(REPLACE(amenities, 'Â·', '.') , 1)
+
+SELECT name, bedroom, bed, bath
+FROM dbo.singapore_listings
+
+SELECT name, bedroom
+FROM dbo.singapore_listings
+WHERE bedroom NOT IN ('%bedroom%')
