79bc2b262406b628b77553ebea113bb170448658
JRLim0902 <83488769+JRLim0902@users.noreply.github.com>
2023-10-05 21:49:34 +0800

    Update Exploration_2.sql

--- Exploration_2.sql
+++ Exploration_2.sql
@@ -1,44 +1,44 @@
 -- relationship of bedroom vs number_of_reviews (most review: 1 bedroom, studio..., support hypothesis that most rentees are work pass holders)
-SELECT bedroom, sum(number_of_reviews) as total_reviews
-from dbo.singapore_listings
-group by bedroom
-order by total_reviews desc
+SELECT bedroom, sum(number_of_reviews) AS total_reviews
+FROM dbo.singapore_listings
+GROUP BY bedroom
+ORDER BY total_reviews DESC
 
 -- relationship of bed vs number_of_reviews (most review: 1 bed, 2 beds..., support hypothesis that most rentees are work pass holders)
-SELECT bed, sum(number_of_reviews) as total_reviews
-from dbo.singapore_listings
-group by bed
-order by total_reviews desc
+SELECT bed, sum(number_of_reviews) AS total_reviews
+FROM dbo.singapore_listings
+GROUP BY bed
+ORDER BY total_reviews DESC
 
 -- relationship of bath vs number_of_reviews (most review: 1 bath..., support hypothesis that most rentees are work pass holders)
-SELECT bath, sum(number_of_reviews) as total_reviews
+SELECT bath, sum(number_of_reviews) AS total_reviews
 from dbo.singapore_listings
-group by bath
-order by total_reviews desc
+GROUP BY bath
+ORDER BY total_reviews DESC
 
 -- relationship of bedroom, bed, bath vs number_of_reviews (most review: 1 bath..., support hypothesis that most rentees are work pass holders)
 SELECT bedroom, bed, bath, sum(number_of_reviews) as total_reviews
-from dbo.singapore_listings
-group by bedroom, bed, bath
-order by total_reviews desc
+FROM dbo.singapore_listings
+GROUP BY bedroom, bed, bath
+ORDER BY total_reviews DESC
 
 SELECT Num_bedroom, Num_bed, Num_bath, sum(number_of_reviews) as total_reviews
-from dbo.singapore_listings
-group by Num_bedroom, Num_bed, Num_bath
-order by total_reviews desc
+FROM dbo.singapore_listings
+GROUP BY Num_bedroom, Num_bed, Num_bath
+ORDER BY total_reviews DESC
 
 -- relationship of neighbourhood_group, room_type, ameneties vs average price 
 SELECT neighbourhood_group, room_type, bedroom, bed, bath, ROUND(AVG(price),2) as avg_price
-from dbo.singapore_listings
-group by neighbourhood_group, room_type, bedroom, bed, bath
-order by room_type, avg_price desc
+FROM dbo.singapore_listings
+GROUP BY neighbourhood_group, room_type, bedroom, bed, bath
+ORDER BY room_type, avg_price DESC
 
 -- relationship of neighbourhood_group, room_type, ameneties vs average price 
 SELECT neighbourhood_group, room_type, Num_bedroom, Num_bed, Num_bath, ROUND(AVG(price),2) as avg_price
-from dbo.singapore_listings
-group by neighbourhood_group, room_type, Num_bedroom, Num_bed, Num_bath
-order by room_type, avg_price desc
+FROM dbo.singapore_listings
+GROUP BY neighbourhood_group, room_type, Num_bedroom, Num_bed, Num_bath
+ORDER BY room_type, avg_price DESC
 
-select name 
-from dbo.singapore_listings
-where bed = '46 beds'
+SELECT name 
+FROM dbo.singapore_listings
+WHERE bed = '46 beds'
